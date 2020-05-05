a0d3bcc65a85070c31de113ed562e51cce400368
chenfengbukeyimi <2590856083@qq.com>
2020-05-05 15:58:44 +0800

    add page profile.vue

--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -1,17 +1,125 @@
 <template>
-  <div>
-    Profile
-  </div>
+  <layout-title-bar
+    left-text="个人资料"
+    left-arrow
+  >
+
+    <section class="baseinfo mt-20">
+      <div @click="routerTo('/profile/avatar')" class="item">
+        <span class="item-left">头像</span>
+        <span class="item-right">
+          <img src="../../assets/image/def_user_icon.jpg" alt="u_cion" class="item-icon"/>
+        </span>
+      </div>
+      <div @click="routerTo('/profile/motto')" class="item">
+        <span class="item-left">姓名</span>
+        <span class="item-right">
+          xiaoyimi
+          <i class="iconfont ml-5">&#xe604;</i>
+        </span>
+      </div>
+      <div @click="show = true" class="item">
+        <span class="item-left">性别</span>
+        <span class="item-right">
+          男
+          <i class="iconfont ml-5">&#xe604;</i>
+        </span>
+      </div>
+      <div class="item">
+        <span class="item-left">小米ID</span>
+        <span class="item-right">
+          15646
+        </span>
+      </div>
+    </section>
+
+    <section class="otherinfo mt-20">
+      <div @click="routerTo('/profile/password')" class="item">
+        <span class="item-left">修改密码</span>
+        <span class="item-right"><i class="iconfont color-secondgray">&#xe604;</i></span>
+      </div>
+      <div @click="routerTo('/profile/address')" class="item">
+        <span class="item-left">收货地址</span>
+        <span class="item-right"><i class="iconfont color-secondgray">&#xe604;</i></span>
+      </div>
+      <div @click="routerTo('/profile/secret')" class="item">
+        <span class="item-left">密保手机</span>
+        <span class="item-right"><i class="iconfont color-secondgray">&#xe604;</i></span>
+      </div>
+      <div @click="routerTo('/profile/userbind')" class="item">
+        <span class="item-left">第三方账户</span>
+        <span class="item-right"><i class="iconfont color-secondgray">&#xe604;</i></span>
+      </div>
+    </section>
+    <van-action-sheet v-model="show" :actions="actions" @select="onSelect" />
+  </layout-title-bar>
 </template>
 
 <script>
+
+import Vue from 'vue'
+import { ActionSheet } from 'vant'
+import 'vant/lib/action-sheet/style'
+import LayoutTitleBar from '@/layout/TitleBar'
+Vue.use(ActionSheet)
+
 export default {
   name: 'Profile',
+  components: { LayoutTitleBar },
   data () {
-    return {}
+    return {
+      baseinfo: [
+        { route: '/adv', text: '小米会员' }
+      ],
+      show: false,
+      actions: [
+        { id: 0, name: '男' },
+        { id: 1, name: '女' },
+        { id: 2, name: '保密' }
+      ]
+    }
+  },
+  methods: {
+    routerLeft () { this.$router.go(-1) },
+    routerTo (route) { this.$router.push({ path: route }) },
+    onSelect (item) {
+      // 默认情况下点击选项时不会自动收起
+      // 可以通过 close-on-click-action 属性开启自动收起
+      this.show = false
+      console.log(item)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.baseinfo, .otherinfo {
+  background-color: #ffffff;
+  .item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #f0f0f0;
+    padding: 0 20px;
+    height: 50px;
+
+    &:last-child { border-bottom: 0; }
+
+    .item-right {
+      font-size: 14px;
+      color: #b9b9b9;
+      .item-icon {
+        border-radius: .5rem;
+        width: 1rem;
+        height: 1rem;
+      }
+    }
+  }
+}
+
+.baseinfo {
+  .item {
+    &:first-child { padding-top: 10px; padding-bottom: 10px; }
+  }
+}
 </style>
